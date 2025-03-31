79606b7a1110cc1120d6d1a3c0ae81678026de3a
BengalAgro2023 <pinak11182@gmail.com>
2025-03-31 12:48:14 +0600

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,25 @@
+const contactForm = document.getElementById('contactForm');
+const confirmationMessage = document.getElementById('confirmationMessage');
+
+contactForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    confirmationMessage.style.display = 'block';
+    contactForm.reset();
+});
+
+
+        function sendMail() {
+   let parms = {
+      name : document.getElementById("name").value,
+      subject : document.getElementById("subject").value,
+      email : document.getElementById("email").value,
+      message : document.getElementById("message").value,
+
+      
+   }
+    
+   emailjs.send("service_ec6v5h2","template_x7pix0e", parms);
+}
+            
+
+
